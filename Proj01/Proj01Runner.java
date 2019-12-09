14c6eb551e07ae456b2b135808e15e0e6a512ecb
carolinekim510 <47015384+carolinekim510@users.noreply.github.com>
2019-12-09 13:33:37 -0600

    2317 Java Intermedia Proj1

---
+++ Proj01/Proj01Runner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Proj01Runner {
+
+	Proj01Runner() {
+		System.out.println("I certify that this program is my own work");
+		System.out.println("and is not the work of others. I agree not");
+		System.out.println("to share my solution with others.");
+		System.out.println("Caroline Kim\n");
+	}
+	
+	public Collection getCollection(String[] myArray){
+		List<String> myWords = Arrays.asList(myArray);
+		Collections.sort(myWords, Collections.reverseOrder());
+		return myWords;
+	}
+	
+}
